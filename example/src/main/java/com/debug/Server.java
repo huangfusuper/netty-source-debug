c5f20f7c6c833e1c197c4e2710782090ab3331e5
huangfusuper <huangfusuper@163.com>
2020-12-08 14:15:03 +0800

    服务测试类

---
+++ example/src/main/java/com/debug/Server.java
@@ -0,0 +1,62 @@
+package com.debug;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.AttributeKey;
+
+/**
+ * 服务端
+ *
+ * @author huangfu
+ * @date 2020年12月8日12:39:47
+ */
+public class Server {
+
+    public static void main(String[] args) {
+        EventLoopGroup boss = new NioEventLoopGroup(1);
+        EventLoopGroup work = new NioEventLoopGroup();
+
+        try{
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(boss,work)
+                    .channel(NioServerSocketChannel.class)
+                    .childOption(ChannelOption.TCP_NODELAY,true)
+                    .childAttr(AttributeKey.newInstance("childAttr"),"childAttrValue")
+                    .handler(new ChannelInboundHandlerAdapter(){
+                        @Override
+                        public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+                            System.out.println("channelRegistered");
+                        }
+
+                        @Override
+                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                            System.out.println("channelActive");
+                        }
+
+                        @Override
+                        public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+                            System.out.println("handlerAdded");
+                        }
+                    })
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+
+                        }
+                    });
+
+            ChannelFuture sync = serverBootstrap.bind(8888).sync();
+            sync.channel().closeFuture().sync();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            boss.shutdownGracefully();
+            work.shutdownGracefully();
+        }
+    }
+}
