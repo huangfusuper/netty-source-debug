94e7ab38576c4a4ffb8ed4e4537ed2169a24b79c
huangfusuper <huangfusuper@163.com>
2020-12-10 09:08:48 +0800

    SocketChannel管道的初始化 注册  绑定源码注释

--- example/src/main/java/com/debug/NioTest.java
+++ example/src/main/java/com/debug/NioTest.java
@@ -1,21 +1,45 @@
 package com.debug;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.spi.SelectorProvider;
 
 public class NioTest {
+    private static ServerSocketChannel serverSocketChannel;
 
     public static void main(String[] args) throws IOException {
+        init();
+        SelectionKey selectionKey = registered();
+        bind(selectionKey);
+        System.out.println(selectionKey.interestOps());
+    }
+
+    public static void bind(SelectionKey selectionKey) throws IOException {
+        serverSocketChannel.bind(new InetSocketAddress(8887));
+        selectionKey.interestOps(SelectionKey.OP_ACCEPT);
+    }
+
+    /**
+     * 注册
+     * @return
+     * @throws IOException
+     */
+    public static SelectionKey registered() throws IOException {
+        Selector open = Selector.open();
+        return serverSocketChannel.register(open, 0);
+    }
+
+    /**
+     * 初始化方法
+     * @throws IOException
+     */
+    private static void init() throws IOException {
         SelectorProvider provider = SelectorProvider.provider();
         ServerSocketChannel serverSocketChannel = provider.openServerSocketChannel();
         serverSocketChannel.configureBlocking(false);
-        Selector open = Selector.open();
-        SelectionKey register = serverSocketChannel.register(open, 0);
-        System.out.println(register.interestOps());
-        register.interestOps(SelectionKey.OP_ACCEPT);
-        System.out.println(register.interestOps());
+        NioTest.serverSocketChannel = serverSocketChannel;
     }
 }
