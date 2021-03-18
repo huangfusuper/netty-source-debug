2b2d13c228a2774ac95cbf692ac5e9ff0dce841f
huangfusuper <huangfusuper@163.com>
2021-03-18 14:37:37 +0800

    ..

--- example/src/main/java/com/wh/nio/buffer/ByteBufferTest.java
+++ example/src/main/java/com/wh/nio/buffer/ByteBufferTest.java
@@ -25,6 +25,7 @@ public class ByteBufferTest {
         heapRam.put("4".getBytes());
         heapRam.flip();
 
+        heapRam.mark();
         byte b1= heapRam.get();
         byte b2 = heapRam.get();
         byte b3 = heapRam.get();
@@ -34,5 +35,24 @@ public class ByteBufferTest {
         System.out.println(b2);
         System.out.println(b3);
         System.out.println(b4);
+
+        heapRam.reset();
+        byte b11= heapRam.get();
+        byte b22 = heapRam.get();
+        byte b33 = heapRam.get();
+        byte b44 = heapRam.get();
+        System.out.println(b11);
+        System.out.println(b22);
+        System.out.println(b33);
+        System.out.println(b44);
+        heapRam.clear();
+        byte[] bytes = "B".getBytes();
+        heapRam.put(4, bytes[0]);
+        heapRam.get();
+        heapRam.get();
+        heapRam.get();
+        heapRam.get();
+        byte b5 = heapRam.get();
+        System.out.println(b5);
     }
 }
